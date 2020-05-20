192839daeaf9d0ea9e4b50852176d750dbfa3b2a
ManayC <manaychoudhary@gmail.com>
2020-05-20 17:54:46 +0530

    creating multiplayer game

--- sketch.js
+++ sketch.js
@@ -1,29 +1,51 @@
 var ball;
+var database;
+var pos;
 
 function setup(){
+    database=firebase.database();
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    var startingPoint=database.ref("ball/position");
+    startingPoint.on("value",refering_readupon,show_error)
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+       position(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+       position(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+       position(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+       position(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function position(x,y){
+
+database.ref("ball/position").set({
+
+x:ball.x+x,y:ball.y+y
+
+})   
+
+}
+function refering_readupon(data){
+
+pos=data.val();
+ball.x=pos.x;
+ball.y=pos.y;
+
+}
+function show_error(){
+
+console.log("show_error");
+
 }
